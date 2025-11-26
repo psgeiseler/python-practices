84cdab2720e710423fee47d38376f3564ef5f67f
Your Name <you@example.com>
2025-11-26 14:57:33 +0100

    Deleted doing

--- story.py
+++ story.py
@@ -3,4 +3,4 @@ verb = input('Name a verb: ')
 adjective = input('Name an adjective: ')
 person = input('Name a person: ')
 
-print(f'Today I went to the {noun}. It was very {adjective}. I saw a {person} doing {verb}.')
+print(f'Today I went to the {noun}. It was very {adjective}. I saw a {person} {verb}.')
