df2f700893166241ec761d2514fb4b0b2d462f18
Your Name <you@example.com>
2025-11-26 14:53:33 +0100

    Added a new project

---
+++ story.py
@@ -0,0 +1,6 @@
+noun = input('Name a noun: ')
+verb = input('Name a verb: ')
+adjective = input('Name an adjective: ')
+person = input('Name a person: ')
+
+print(f'Today I went to the {noun}. It was very {adjective}. I saw a {person} doing {verb}.')
